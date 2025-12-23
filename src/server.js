74b44586fe75dad9931cd007944b99ace90ec371
krishnaVerma1302 <17krishnaverma@gmail.com>
2025-12-23 14:59:29 +0530

    3nd fix cos url in server

--- src/server.js
+++ src/server.js
@@ -10,7 +10,9 @@ connectDB()
 const app = express();
 app.use(cors(
   {
-    origin:["http://localhost:5173","https://neeraj-traders.vercel.app"],
+    origin:["http://localhost:5173",
+      "https://neeraj-traders.vercel.app",
+      "neeraj-traders-krishnaverma13s-projects.vercel.app"],
     credentials: true
   }
 ));
